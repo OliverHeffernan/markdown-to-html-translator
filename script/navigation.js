7da69209b687e87ff31bc4e8feddd6555792c039
OliverHeffernan <oliverheffernan@icloud.com>
2024-09-21 17:10:38 +1200

    added ability to switch orientation, and automatically switches on load when on phone.

--- script/navigation.js
+++ script/navigation.js
@@ -1,11 +1,26 @@
 function preview() {
-    document.getElementById("highlight").className = "preview";
+    document.querySelector("#viewMode #highlight").className = "left";
     document.getElementById("htmlSide").style.display = "block";
     document.getElementById("codeSide").style.display = "none";
 }
 
 function viewCode() {
-    document.getElementById("highlight").className = "viewCode";
+    document.querySelector("#viewMode #highlight").className = "right";
     document.getElementById("htmlSide").style.display = "none";
     document.getElementById("codeSide").style.display = "block";
+}
+
+if (window.innerWidth < window.innerHeight) {
+    horizontal();
+}
+
+
+function horizontal() {
+    document.querySelector("#orMode #highlight").className = "right";
+    document.getElementById("sides").className = "horizontal";
+}
+
+function vertical() {
+    document.querySelector("#orMode #highlight").className = "left";
+    document.getElementById("sides").className = "vertical";
 }
